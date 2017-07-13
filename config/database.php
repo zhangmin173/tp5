ee2f4a615c948e62e4420e32931a3742acd26f96
zhangmin <303890562@qq.com>
2017-07-13 09:08:20 +0800

    ignore database

--- config/database.php
+++ config/database.php
@@ -2,13 +2,13 @@
 
 return [
     // 服务器地址
-    'hostname'        => '',
+    'hostname'        => '127.0.0.1',
     // 数据库名
-    'database'        => '',
+    'database'        => 'dev',
     // 数据库用户名
-    'username'        => '',
+    'username'        => 'root',
     // 数据库密码
-    'password'        => '',
+    'password'        => 'root',
     // 数据库表前缀
     'prefix'          => 'tb',
     // 数据库调试模式
